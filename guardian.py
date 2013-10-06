3347bb83f91522e1d22ae69e710b15d281fce4ee
Daniel Waardal <waawal@boom.ws>
2013-10-06 13:06:47 +0200

    return none if no results

--- guardian.py
+++ guardian.py
@@ -11,7 +11,7 @@ class Guardian(object):
         payload = {}
         for k, v in kw.items():
             payload[k.replace('_','-')] = v
-        return requests.get(self.ITEM+id, params=payload).json()['response']['content']
+        return requests.get(self.ITEM+id, params=payload).json()['response'].get('content')
 
     def search(self, q=None, **kw):
         if  q is None:
@@ -20,13 +20,13 @@ class Guardian(object):
             payload = {'q': q}
         for k, v in kw.items():
             payload[k.replace('_','-')] = v
-        return requests.get(self.SEARCH, params=payload).json()['response']['results']
+        return requests.get(self.SEARCH, params=payload).json()['response'].get('results')
 
     def tags(self, **kw):
         payload = {}
         for k, v in kw.items():
             payload[k.replace('_','-')] = v
-        return requests.get(self.TAGS, params=payload).json()['response']['results']
+        return requests.get(self.TAGS, params=payload).json()['response'].get('results')
 
     def __call__(self, q, **kw):
         return self.search(q=q, **kw)
